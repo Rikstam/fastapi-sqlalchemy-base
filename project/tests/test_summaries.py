2c661654ec1dc4f9068c92d5c8edbe13f230a542
Riku Kestilä <rikukestila@gmail.com>
2025-05-06 10:10:48 +0300

    Parametrize tests

--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -140,87 +140,82 @@ async def test_update_summary(test_app_with_db):
     assert response_dict["created_at"]
 
 
+@pytest.mark.parametrize(
+    "summary_id, payload, status_code, detail",
+    [
+        [
+            999,
+            {"url": "https://foo.bar/", "summary": "updated!"},
+            404,
+            "Summary not found",
+        ],
+        [
+            0,
+            {"url": "https://foo.bar/", "summary": "updated!"},
+            422,
+            [
+                {
+                    "type": "greater_than",
+                    "loc": ["path", "id"],
+                    "msg": "Input should be greater than 0",
+                    "input": "0",
+                    "ctx": {"gt": 0},
+                }
+            ],
+        ],
+        [
+            1,
+            {},
+            422,
+            [
+                {
+                    "type": "missing",
+                    "loc": ["body", "url"],
+                    "msg": "Field required",
+                    "input": {},
+                },
+                {
+                    "type": "missing",
+                    "loc": ["body", "summary"],
+                    "msg": "Field required",
+                    "input": {},
+                },
+            ],
+        ],
+        [
+            1,
+            {"url": "https://foo.bar/"},
+            422,
+            [
+                {
+                    "type": "missing",
+                    "loc": ["body", "summary"],
+                    "msg": "Field required",
+                    "input": {"url": "https://foo.bar/"},
+                }
+            ],
+        ],
+    ],
+)
 @pytest.mark.anyio
-async def test_update_summary_incorrect_id(test_app_with_db):
+async def test_update_summary_invalid(
+    test_app_with_db, summary_id, payload, status_code, detail
+):
     response = await test_app_with_db.put(
-        "/summaries/999/",
-        data=json.dumps({"url": "https://foo.bar/", "summary": "updated!"}),
+        f"/summaries/{summary_id}/", data=json.dumps(payload)
     )
-    assert response.status_code == 404
-    assert response.json()["detail"] == "Summary not found"
-
-    response = await test_app_with_db.put(
-        f"/summaries/0/",
-        data=json.dumps({"url": "https://foo.bar/", "summary": "updated!"})
-    )
-    assert response.status_code == 422
-    assert response.json() == {
-        "detail": [
-            {
-                "ctx": {"gt": 0},
-                "input": "0",
-                "loc": ["path", "id"],
-                "msg": "Input should be greater than 0",
-                "type": "greater_than",
-            }
-        ]
-    }
+    assert response.status_code == status_code
+    print(response.json()["detail"])
+    assert response.json()["detail"] == detail
 
 
 @pytest.mark.anyio
-async def test_update_summary_invalid_json(test_app_with_db):
-    response = await test_app_with_db.post(
-        "/summaries/", data=json.dumps({"url": "https://foo.bar/"})
-    )
-    summary_id = response.json()["id"]
-
+async def test_update_summary_invalid_url(test_app_with_db):
     response = await test_app_with_db.put(
-        f"/summaries/{summary_id}/", data=json.dumps({})
+        "/summaries/1/",
+        data=json.dumps({"url": "invalid://url", "summary": "updated!"}),
     )
     assert response.status_code == 422
-    assert response.json() == {
-        "detail": [
-            {
-                "input": {},
-                "loc": ["body", "url"],
-                "msg": "Field required",
-                "type": "missing",
-            },
-            {
-                "input": {},
-                "loc": ["body", "summary"],
-                "msg": "Field required",
-                "type": "missing",
-            },
-        ]
-    }
-
-
-@pytest.mark.anyio
-async def test_update_summary_invalid_keys(test_app_with_db):
-    response = await test_app_with_db.post(
-        "/summaries/", data=json.dumps({"url": "https://foo.bar/"})
+    assert (
+        response.json()["detail"][0]["msg"] == "URL scheme should be 'http' or 'https'"
     )
-    summary_id = response.json()["id"]
-
-    response = await test_app_with_db.put(
-        f"/summaries/{summary_id}/", data=json.dumps({"url": "https://foo.bar/"})
-    )
-    assert response.status_code == 422
-    assert response.json() == {
-        "detail": [
-            {
-                "input": {"url": "https://foo.bar/"},
-                "loc": ["body", "summary"],
-                "msg": "Field required",
-                "type": "missing",
-            }
-        ]
-    }
-
-    response = await test_app_with_db.put(
-        f"/summaries/{summary_id}/",
-        data=json.dumps({"url": "invalid://url", "summary": "updated!"})
-    )
-    assert response.status_code == 422
-    assert response.json()["detail"][0]["msg"] == "URL scheme should be 'http' or 'https'"
